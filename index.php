1b59f74ed04aab8d29aa26585391bc9699786e52
Joao Alves <joaoalvesjc16@gmail.com>
2019-03-02 00:18:41 -0300

    primeiro

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Sistema de Chamadas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo_mobile.css">
+    <script src="lib/js/controle.js"></script>
+</head>
+<body>
+    <div class="envelope">
+        <div class="linha">
+            <div class="box_pedido urgente">
+                <div class="linha">
+                    <span class="titulo_box">Pedidos Urgentes <span class="titulo_box_quant">(06)</span></span>
+                </div>
+
+                <div class="box_solicitacao">
+                    <div class="linha_vertical">
+                        <span class="titulo_cartao">
+                            Titulo Cartão
+                        </span>
+
+                        <div class="img_cartao">
+                            <img src="img/img_cartao.jpg" alt="img_cartao" />
+                        </div>
+                    </div>
+                    
+                    <div class="descricao_cartao">
+                        Aliqua wiiiii adipisicing ullamco dolor commodo reprehenderit chasy la bodaaa uuuhhh. Minim tempor poulet tikka masala po kass tulaliloo aaaaaah tatata bala tu po kass.
+                    </div>
+                </div>
+            </div>
+            
+            <!-- <div class="box_pedido andamento">
+                <div class="linha">
+                    <span class="titulo_box">Pedidos Em Andamento</span>
+                </div>
+                <div class="box_solicitacao">
+                    
+                </div>
+            </div>
+
+            <div class="box_pedido concluido">
+                <div class="linha">
+                    <span class="titulo_box">Pedidos Concluído</span>
+                </div>
+                <div class="box_solicitacao">
+                    
+                </div>
+            </div> -->
+        </div>
+    </div>
+</body>
+</html>
