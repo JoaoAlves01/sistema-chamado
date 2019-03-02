3cd57fb1bed27037e2c6cfdfc3204f543c4e63b7
DiogoDionizio <tec.diogoguima@gmail.com>
2019-03-02 01:30:35 -0300

    conexao
    alteração no nome do banco de dados

--- config/conexao.php
+++ config/conexao.php
@@ -2,7 +2,7 @@
 	function model_conexao(){
 		//Conexao com o servidor
 		//Servidor, usuario, senha, DB
-		$conection = new mysqli("localhost", "root", "", "tcc01-sae");
+		$conection = new mysqli("localhost", "root", "", "si_chamado");
 		//define o idioma para portugues
 		$conection->set_charset("utf8");
 
