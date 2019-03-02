23c1e89b0ececeac39672d01b86981d5e7d2180b
Joao Alves <joaoalvesjc16@gmail.com>
2019-03-02 18:14:30 -0300

    criacao da tela de comentario

---
+++ cabecalho_card.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Titulo Cartão</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo_mobile.css">
+    <script src="lib/js/jquery.js"></script> 
+    <script src="lib/js/controle.js"></script> 
+</head>
+<body>
+    <div class="faixa_info urgente">
+        <div class="box_pontos" id="faixa_cartao">
+            <div class="box_trofeu">
+                <i class="fa fa-th-list" aria-hidden="true"></i>
+            </div>
+            <span class="titulo_cartao">Título Cartão</span>
+        </div>
+
+        <div class="box_pontos" id="faixa_voltar">
+            <div class="box_trofeu">
+                <i class="fa fa-close" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="envelope">
