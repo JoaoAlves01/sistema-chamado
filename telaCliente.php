1809e6b51cd1cd27f219e07e4ad023916501ec29
Joao Alves <joaoalvesjc16@gmail.com>
2019-03-03 00:33:49 -0300

    criacao da pagina de visualizar pedidos

---
+++ telaCliente.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+    <title>Visualizar Pedidos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="lib/css/estilo_mobile.css">
+    <script src="lib/js/jquery.js"></script> 
+    <script src="lib/js/controle.js"></script> 
+<body>
+    <div class="envelope">
+        <div class="box_visul_pedido">
+            <div class="linha_vertical">
+                <div class="tipo_pedido"></div>
+
+                <div class="titulo_pedido">
+                    <span class="titulos_box_pedido">Título</span>
+                </div>
+
+                <div class="data_pedido">
+                    <span class="titulos_box_pedido">Data</span>
+                </div>
+
+                <div class="tecnico_resp_pedido">
+                    <span class="titulos_box_pedido">Técnico Responsável</span>
+                </div>
+
+                <div class="status_pedido">
+                    <span class="titulos_box_pedido">Status</span>
+                </div>
+
+                <div class="botao_pedido"></div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
