a9a280c20b529c03c5a80b8446ded2a4e6e92315
DiogoDionizio <tec.diogoguima@gmail.com>
2019-03-03 18:06:14 -0300

    pagina cartao, comentarios excluir

--- carregarComentarios.php
+++ carregarComentarios.php
@@ -7,7 +7,6 @@ include("controller.php");
 $_SESSION['chamado'] = listarChamadoId($_SESSION['id']);
 
 $usuarioChamado = listarUsuario($_SESSION['chamado'][1]);
-var_dump($usuarioChamado);
 
 $listarComentarios = listarComentarios($_SESSION['id']);
 
@@ -16,9 +15,10 @@ while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
                 if($resul[2] == $_SESSION['chamado'][1]){ ?>
 
                     <!-- Container direito -->
-                    <div class="container_comentario" id="valorAqui" data-id="valorAqui">
+                    
 
                         <?php if ($resul[2] == $_SESSION['idUser']) { ?>
+                            <div class="container_comentario esquerda_comentario" id="<?php echo $resul[5];?>" data-id="<?php echo $resul[5];?>">
                             <form method="POST" action="">
                                 <div class="alinha_botao">
                                     <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
@@ -26,7 +26,11 @@ while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
                                     <button type="button" class="botao editar_cartao_comentario botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
                                 </div>
                             </form>
-                        <?php } ?>
+                        <?php } else { ?>
+
+                        <div class="container_comentario" id="<?php echo $resul[5];?>" data-id="<?php echo $resul[5];?>">
+
+                        <?php } ?>    
 
                         <div class="linha">
                             <div class="img_cartao img_comentario">
@@ -46,16 +50,20 @@ while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
             <?php 
                 } else { ?>
                     <!-- Container esquerdo -->
-                    <div class="container_comentario esquerda_comentario" id="valorAqui1" data-id="valorAqui1">
 
-                        <?php if ($resul[2] == $_SESSION['idUser']) { ?>
+                    <?php if ($resul[2] == $_SESSION['idUser']) { ?>
+                    <div class="container_comentario esquerda_comentario" id="<?php echo $resul[5];?>" data-id="<?php echo $resul[5];?>">
                             <form method="POST" action="">
                                 <div class="alinha_botao">
-                                    <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario" value=""><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario" value="<?php echo $resul[5];?>"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
                                     <button type="button" class="botao cancelar_cartao_comentario botao_icone_comentario" id="cancelar_cartao_comentario" name="cancelar_cartao_comentario"><i class="fa fa-close" aria-hidden="true"></i></button>
                                     <button type="button" class="botao editar_cartao_comentario botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
                                 </div>
                             </form>
+                        <?php } else { ?>
+
+                        <div class="container_comentario esquerda_comentario">
+
                         <?php } ?>
 
                         <div class="linha">
