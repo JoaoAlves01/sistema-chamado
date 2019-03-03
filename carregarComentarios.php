aab86cd9ed7dbf4a84786fcbee3557c483443294
Joao Alves <joaoalvesjc16@gmail.com>
2019-03-03 17:13:09 -0300

    ajuste na tela de comentarioa

--- carregarComentarios.php
+++ carregarComentarios.php
@@ -13,13 +13,14 @@ while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
                 if($resul[2] == $_SESSION['chamado'][1]){ ?>
 
                     <!-- Container direito -->
-                    <div class="container_comentario">
+                    <div class="container_comentario" id="valorAqui" data-id="valorAqui">
 
                         <?php if ($resul[2] == $_SESSION['idUser']) { ?>
                             <form method="POST" action="">
                                 <div class="alinha_botao">
-                                    <button type="submit" class="botao botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    <button type="submit" class="botao botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao cancelar_cartao_comentario botao_icone_comentario" id="cancelar_cartao_comentario" name="cancelar_cartao_comentario"><i class="fa fa-close" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao editar_cartao_comentario botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
                                 </div>
                             </form>
                         <?php } ?>
@@ -28,24 +29,28 @@ while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
                             <div class="img_cartao img_comentario">
                                 <img src="../images/perfil/<?php echo $usuarioChamado[5]; ?>" alt="img_cartao" />
                             </div>
-                            <span class="nome_usuario_comentario"><?php echo $usuarioChamado[2];  ?></span>
-                            <span class="data_usuario_comentario"><?php echo dataHoraBras($resul[4]);  ?></span>
-                            <p class="comentario_cartao">
-                                <?php echo $resul[3];  ?>
-                            </p>
+                            <div class="balao">
+                                <span class="nome_usuario_comentario"><?php echo $usuarioChamado[2];?></span>
+                                <span class="data_usuario_comentario"><?php echo dataHoraBras($resul[4]);?></span>
+                                <div class="container_input_comentario">
+                                    <input type="text" class="campo_sistema digitar_comentario" id="editar_comentario_balao" name="texto" placeholder="Digite aqui..." />
+                                </div>
+                                <p class="comentario_cartao"><?php echo $resul[3];?></p>
+                            </div>
                         </div>
                     </div>
                     <!-- Fim Container esquerdo -->
             <?php 
                 } else { ?>
                     <!-- Container esquerdo -->
-                    <div class="container_comentario esquerda_comentario">
+                    <div class="container_comentario esquerda_comentario" id="valorAqui1" data-id="valorAqui1">
 
                         <?php if ($resul[2] == $_SESSION['idUser']) { ?>
                             <form method="POST" action="">
                                 <div class="alinha_botao">
-                                    <button type="submit" class="botao botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                    <button type="submit" class="botao botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario" value=""><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao cancelar_cartao_comentario botao_icone_comentario" id="cancelar_cartao_comentario" name="cancelar_cartao_comentario"><i class="fa fa-close" aria-hidden="true"></i></button>
+                                    <button type="button" class="botao editar_cartao_comentario botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
                                 </div>
                             </form>
                         <?php } ?>
@@ -54,11 +59,14 @@ while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
                             <div class="img_cartao img_comentario">
                                 <img src="../images/perfil/<?php $usuario = listarUsuario($resul[2]); echo $usuario[5]; ?>" alt="img_cartao" />
                             </div>
-                            <span class="nome_usuario_comentario"><?php echo $usuario[2]; ?></span>
-                            <span class="data_usuario_comentario"><?php echo dataHoraBras($resul[4]);  ?></span>
-                            <p class="comentario_cartao">
-                                <?php echo $resul[3];  ?>
-                            </p>
+                            <div class="balao">
+                                <span class="nome_usuario_comentario"><?php echo $usuario[2]; ?></span>
+                                <span class="data_usuario_comentario"><?php echo dataHoraBras($resul[4]);?></span>
+                                <div class="container_input_comentario">
+                                    <input type="text" class="campo_sistema digitar_comentario" id="editar_comentario_balao" name="texto" placeholder="Digite aqui..." />
+                                </div>
+                                <p class="comentario_cartao"><?php echo $resul[3];?></p>
+                            </div>
                         </div>
                     </div>
                     <!-- Fim Container esquerdo -->                
--- cartao.php
+++ cartao.php
@@ -67,69 +67,9 @@
             </form>
             <?php } ?>
 
-<<<<<<< HEAD
             <div id="caixaComentarios">
 
             </div>
-=======
-            <?php while ($resul = $listarComentarios->fetch_array(MYSQLI_NUM)) {
-                
-                if($resul[2] == $chamado[1]){ ?>
-
-                    <!-- Container direito -->
-                    <div class="container_comentario" id="valorAqui" data-id="valorAqui">
-                        <form method="POST" action="">
-                            <div class="alinha_botao">
-                                <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                <button type="button" class="botao cancelar_cartao_comentario botao_icone_comentario" id="cancelar_cartao_comentario" name="cancelar_cartao_comentario"><i class="fa fa-close" aria-hidden="true"></i></button>
-                                <button type="button" class="botao editar_cartao_comentario botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
-                            </div>
-                        </form>
-                        <div class="linha">
-                            <div class="img_cartao img_comentario">
-                                <img src="../images/perfil/<?php echo $usuarioChamado[5]; ?>" alt="img_cartao" />
-                            </div>
-                            <div class="balao">
-                                <span class="nome_usuario_comentario"><?php echo $usuarioChamado[2];  ?></span>
-                                <span class="data_usuario_comentario"><?php echo dataHoraBras($resul[4]);  ?></span>
-                                
-                                <div class="container_input_comentario">
-                                    <input type="text" class="campo_sistema digitar_comentario" id="editar_comentario_balao" name="texto" placeholder="Digite aqui..." />
-                                </div>
-
-                                <p class="comentario_cartao"><?php echo $resul[3];?></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Fim Container esquerdo -->
-            <?php 
-                } else { ?>
-                    <!-- Container esquerdo -->
-                    <div class="container_comentario esquerda_comentario" id="valorAqui1" data-id="valorAqui1">
-                        <form method="POST" action="">
-                            <div class="alinha_botao">
-                                <button type="button" class="botao deletar_cartao_comentario botao_icone_comentario" id="deletar_cartao_comentario" name="deletar_cartao_comentario" value=""><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                <button type="button" class="botao cancelar_cartao_comentario botao_icone_comentario" id="cancelar_cartao_comentario" name="cancelar_cartao_comentario"><i class="fa fa-close" aria-hidden="true"></i></button>
-                                <button type="button" class="botao editar_cartao_comentario botao_icone_comentario" id="editar_cartao_comentario" name="editar_cartao_comentario"><i class="fa fa-pencil" aria-hidden="true"></i></button>
-                            </div>
-                        </form>
-                        <div class="linha">
-                            <div class="img_cartao img_comentario">
-                                <img src="../images/perfil/<?php $usuario = listarUsuario($resul[2]); echo $usuario[5]; ?>" alt="img_cartao" />
-                            </div>
-                            <div class="balao">
-                                <span class="nome_usuario_comentario"><?php echo $usuario[2]; ?></span>
-                                <span class="data_usuario_comentario"><?php echo dataHoraBras($resul[4]);  ?></span>
-                                <div class="container_input_comentario">
-                                    <input type="text" class="campo_sistema digitar_comentario" id="editar_comentario_balao" name="texto" placeholder="Digite aqui..." />
-                                </div>
-                                <p class="comentario_cartao"><?php echo $resul[3];?></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Fim Container esquerdo -->                
-            <?php } } ?>
->>>>>>> 71250b03ac1ec21ef6f74b7d97a021040ae8b09b
 
             <div class="container_novo_comentario">
                 <div class="linha_vertical">
